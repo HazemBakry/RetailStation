fb1f7165147f4aead4c97d93f6ba40d15eb56852
hazem bakry <enghazembakry20@gmail.com>
2024-08-13 20:39:16 +0300

    add home icon to side menu

--- MasterErp.Entities/DTOs/Auth/AuthModel.cs
+++ MasterErp.Entities/DTOs/Auth/AuthModel.cs
@@ -13,6 +13,8 @@ namespace MasterErp.Entities.DTOs.Auth
         public string UserName { get; set; }
         public string UserId { get; set; }
         public string FullName { get; set; }
+        public string BranchNameAr { get; set; }
+        public string BranchNameEn { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string ImageUrl { get; set; }
