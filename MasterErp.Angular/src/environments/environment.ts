679a17d7ece2b51ec23069c15ebaa308917ffa5c
Elsayed Elassal <elsayed.elassal@z2data.com>
2025-01-20 22:25:30 +0200

    update environemnt

--- MasterErp.Angular/src/environments/environment.ts
+++ MasterErp.Angular/src/environments/environment.ts
@@ -5,11 +5,13 @@
 export const environment = {
   production: false,
   apiURL:'http://localhost:63246/api/',
-  authApi:'http://localhost:63246/api/',
   systemUrl: 'http://localhost:4200/',
-  authServerUrl: 'http://localhost:4400/',
   appId: 'e5b35f76-bdd3-4e93-8038-89668f4ff4bc',
   defaultUserPassword: '0000',
+
+  //login system
+  authApi:'https://loginsso.infood.ai/api/',
+  authServerUrl: 'https://login.infood.ai/',
 };
 
 
