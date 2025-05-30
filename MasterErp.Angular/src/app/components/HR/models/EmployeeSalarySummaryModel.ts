d152866bbb6ae3b5e4a00ce2cf14d37e5fab51cc
Elsayed Elassal <elsayed.elassal@z2data.com>
2025-05-31 01:13:09 +0300

    update salaries

--- MasterErp.Angular/src/app/components/HR/models/EmployeeSalarySummaryModel.ts
+++ MasterErp.Angular/src/app/components/HR/models/EmployeeSalarySummaryModel.ts
@@ -1,22 +1,56 @@
+
+export interface EmployeeSalarySummaryModel {
+    totalCount: number | null;
+
+}
+
 export interface EmployeeSalarySummaryModel {
     totalCount: number | null;
     employeeId: number | null;
     employeeCode: string;
     bankAccountNumber: string;
+    bank: string;
+    jobTitle: string;
     employeeNameEN: string;
     employeeNameAR: string;
     branchId: number | null;
     branchNameEN: string;
     branchNameAR: string;
+    deductions: number | null;
+    advances: number | null;
+    penalties: number | null;
+    overtime: number | null;
+    grossSalary: number | null;
     totalSalary: number | null;
+    basicSalary: number | null;
+    extraSalary: number | null;
+    transportation: number | null;
+    housingAllowance: number | null;
+    mobileAllowance: number | null;
+    workNature: number | null;
+    mealAllowance: number | null;
+    other: number | null;
     dailySalary: number | null;
+    totalDeductions: number | null;
     calculatedSalary: number | null;
     netSalary: number | null;
     presentDays: number | null;
     offDays: number | null;
     sickDays: number | null;
     absentDays: number | null;
-    effectiveWorkingDays: number | null;
-    deductions: number | null;
-    advances: number | null;
+    totalWorkingDays: number | null;
+
+
+
+
+    isPaid: boolean | null;
+    isGossi: boolean | null;
+    totalGrossSalary: number | null;
+    hRManagerApprove: boolean | null;
+    financeManagerApprove: boolean | null;
+    auditingApprove: boolean | null;
+    executiveManagerApprove: boolean | null;
+    generalManagerApprove: boolean | null;
+    visaPayment: number | null;
+    cashPayment: number | null;
 }
--- MasterErp.Entities/DTOs/HR/EmployeeSalarySummaryModel.cs
+++ MasterErp.Entities/DTOs/HR/EmployeeSalarySummaryModel.cs
@@ -13,17 +13,37 @@ namespace MasterErp.Entities.DTOs.HR
 
         public int? EmployeeId { get; set; }
         public string EmployeeCode { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string Bank { get; set; }
+        public string JobTitle { get; set; }
         public string EmployeeNameEN { get; set; }
         public string EmployeeNameAR { get; set; }
 
 
         public int? BranchId { get; set; }
         public string BranchNameEN { get; set; }
-        public string BranchNameAR { get; set; }
+        public string BranchNameAR { get; set; }        
+        public string JobNameEN { get; set; }
+        public string JobNameAR { get; set; }
 
 
+        public double? Deductions { get; set; }
+        public double? Advances { get; set; }
+        public double? Penalties { get; set; }
+        public double? Overtime { get; set; }
+
+        public double? GrossSalary { get; set; }
         public double? TotalSalary { get; set; }
+        public double? BasicSalary { get; set; }
+        public double? ExtraSalary { get; set; }
+        public double? Transportation { get; set; }
+        public double? HousingAllowance { get; set; }
+        public double? MobileAllowance { get; set; }
+        public double? WorkNature { get; set; }
+        public double? MealAllowance { get; set; }
+        public double? Other { get; set; }
         public decimal? DailySalary { get; set; }
+        public decimal? TotalDeductions { get; set; }
         public decimal? CalculatedSalary { get; set; }
         public decimal? NetSalary { get; set; }
 
@@ -33,10 +53,8 @@ namespace MasterErp.Entities.DTOs.HR
         public int? SickDays { get; set; }
         public int? AbsentDays { get; set; }
 
-        public int? EffectiveWorkingDays { get; set; }
+        public int? TotalWorkingDays { get; set; }
 
-        public double? Deductions { get; set; }
-        public double? Advances { get; set; }
     }
 
 }
