2fb7007c3de1ba2ea14fb0bb3831bea414650291
hazem bakry <enghazembakry20@gmail.com>
2025-11-12 14:31:43 +0200

    add defaultImage key in environment.prod

--- RetailStation.Angular/src/environments/environment.prod.ts
+++ RetailStation.Angular/src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   systemUrl: 'https://retail.infood.ai/',
   defaultUserPassword: '0000',
   authApi: 'https://retail.infood.ai/webapi/api/',
+  defaultImage: 'assets/images/retail-default.jpg',
+
   // authServerUrl: 'https://app.infood.ai',
 };
